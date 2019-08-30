c5fac959c7627fe2855e85c956d064d0a92608a1
Ibraheem Alsaghier <Ibraheem Alsaghier@DESKTOP-85163MR.wireless-1x.unc.edu>
2019-08-30 15:07:12 -0400

    First submission

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -7,8 +7,78 @@ public class A1Adept {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
+		//comment
 		// Your code follows here.
+		int num = scan.nextInt();
+		
+		// stores products and their prices
+		String[] prod = new String[num];
+		double [] price = new double[num];
+		
+		for (int i = 0; i < num; i++)
+		{
+			prod[i] = scan.next();
+			price[i] = scan.nextDouble();
+			
+			
+		}
+		
+		// creates array with customer names 
+		int num2 = scan.nextInt();
+		String[] first = new String [num2];
+		String[]last = new String [num2];
+		// total money paid
+		double [] total = new double [num2];
+		//number of items in cart
+		int cart = 0;
+		//name of items
+		String item = " ";
+		// number of items
+		int quantity = 0;
+		
+		for(int i = 0; i < num2; i++)
+		{
+		first[i] = scan.next();
+		last[i] = scan.next();
+		cart = scan.nextInt();
+			for(int n = 0; n < cart; n++)
+			{
+			quantity = scan.nextInt();
+			item = scan.next();
+				for(int q = 0; q < num; q++)
+				{
+					if (item.equals(prod[q]))
+					{
+						total[i] += quantity * price[q];
+					}
+				}
+			}
+		}
+		int largest = 0;
+		int smallest = 0;
+	for (int p = 0; p < num2; p++) {
+		if (total[p] > total[largest] )
+		{
+			largest = p;
+		}
 		
 	}
+	for(int l = 0; l < num2; l++)
+	{
+		if(total [l] < total [smallest])
+		{
+			smallest = l;
+		}
+	}
+	double average = 0;
+	for (int a = 0; a < num2; a++)
+	{
+		average += total [a];
+	}
+		average = average/num2;
+		System.out.println("Biggest: " + first [largest] +" " + last[largest] + " (" + total [largest] + ")" );
+		System.out.println("Smallest: " + first [smallest] + " " + last[smallest] + " (" + total [smallest] +")");
+		System.out.println("Average: (" + average + ")");
+	}
+	
 }
--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,54 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		// Finds the number of customers
+		int cust = scan.nextInt();
+		
+		System.out.println(cust);
+		// create an array of customer first names and last names.
+		
+		String [] first = new String [cust];
+		String [] last = new String [cust];
+		// array that stores the money spent by each customer
+		double[] value = new double[cust];
+		//create a variable that stores number of items
+		
+		int items = 0;
+		
+		for( int i = 0; i < cust; i++) {
+		first [i] = scan.next();
+		
+		last [i] = scan.next();
+		
+		items = scan.nextInt();
+		value [i] = value (items);
+		
+		}
+		
+		for (int i = 0; i < cust; i++)
+		{
+			System.out.println( first [i].charAt(0)+ ". "  + last[i]+" " +  value[i]  );
+		}
+		scan.close();
+	}
+	public static double value (int length)
+	{
+		//comment
+		double value = 0;
+		int quantity = 0;
+		String product = " ";
+		double price = 0;
+		Scanner scan2 = new Scanner(System.in);
+		for(int i = 0; i < length; i++)
+		{
+			//finds value of all items
+			quantity = scan2.nextInt();
+			product = scan2.next();
+			price = scan2.nextDouble();
+			value += quantity * price;
+			
+		}
+		
+		return value;
 	}
 }
